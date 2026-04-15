178c1a9226cbc25d45d57a9c7467d164567658ed
JoeDev37 <alonejoe367@gmail.com>
2026-04-15 17:55:56 +0300

    phaser-first-game(coding)

--- src/sceanes/gameScreen.js
+++ src/sceanes/gameScreen.js
@@ -4,6 +4,9 @@ export default class gameScreen extends Phaser.Scene {
 
     init() {
         this.poddrightVelocity = new Phaser.Math.Vector2(0, 0)
+
+        this.leftScore = 0;
+        this.rightScore = 0;
     }
 
     preload() {
@@ -12,24 +15,39 @@ export default class gameScreen extends Phaser.Scene {
 
     create() {
 
+        this.physics.world.setBounds(-100, 0, 1000, 500)
+
         this.ball = this.add.circle(400, 250, 10, 0xffffff, 1);
         this.physics.add.existing(this.ball)
         this.ball.body.setBounce(1, 1)
 
         this.ball.body.setCollideWorldBounds(true, 1, 1)
 
-        this.ball.body.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
+        this.resetBall()
 
-       this.poddleft = this.add.rectangle(30, 243, 40, 100, 0xffffff, 1);
-       this.physics.add.existing(this.poddleft, true);
+        // this.ball.body.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200));
 
-       this.poddright = this.add.rectangle(770, 243, 40, 100, 0xffffff, 1)
-       this.physics.add.existing(this.poddright, true);
+        this.poddleft = this.add.rectangle(30, 243, 40, 100, 0xffffff, 1);
+        this.physics.add.existing(this.poddleft, true);
+
+        this.poddright = this.add.rectangle(770, 243, 40, 100, 0xffffff, 1)
+        this.physics.add.existing(this.poddright, true);
 
         this.physics.add.collider(this.ball, this.poddleft);
         this.physics.add.collider(this.ball, this.poddright);
 
+        const scoreSize = {
+            fontSize: 48
+        }
+
+        this.leftScore = this.add.text(300, 125, '0', scoreSize)
+        .setOrigin(0.5, 0.5)
+
+        this.rightScore = this.add.text(500, 375, '0', scoreSize)
+        .setOrigin(0.5, 0.5)
+
         this.cursors = this.input.keyboard.createCursorKeys()
+
     }
 
     update() {
@@ -65,5 +83,35 @@ export default class gameScreen extends Phaser.Scene {
 
         this.poddright.y += this.poddrightVelocity.y;
         this.poddright.body.updateFromGameObject()
+
+
+        if (this.ball.x < -30) {
+            // scored the left side
+            
+            this.resetBall();
+            this.incrementLeftScore();
+
+        } else if (this.ball.x > 830) {
+        //scored the right side
+
+        this.resetBall()
+
+        }
+        
     }
+
+    incrementLeftScore() {
+        this.leftScore++
+        this.leftScoreText = setText(this.leftScore)
+    }
+
+    resetBall() {
+
+        // this.ball.setPosition(400, 250)
+
+        const angle = Phaser.Math.Between(1, 360)
+        const vec = this.physics.velocityFromAngle(angle, 200)
+        this.ball.body.setVelocity(vec.x, vec.y)
+    }
+        
 }
